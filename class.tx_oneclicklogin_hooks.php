d6369c5f900c8e7b5de00fe1c274182831afda5a
phpprof <phpprof@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-27 14:03:28 +0000

    Compatibility with TYPO3 4.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/oneclicklogin/trunk@61177 735d13b6-9817-0410-8766-e36946ffe9aa

--- class.tx_oneclicklogin_hooks.php
+++ class.tx_oneclicklogin_hooks.php
@@ -1,17 +1,41 @@
 <?php
 
-class tx_oneclicklogin_hooks{
+class tx_oneclicklogin_hooks {
 
-	function addLoginNews(&$params, &$reference) {
-		global $TYPO3_CONF_VARS;
-		$_EXTCONF = unserialize($TYPO3_CONF_VARS['EXT']['extConf']['oneclicklogin']);
-		
-		if($_EXTCONF['use_devIPmask'] && !t3lib_div::cmpIP(t3lib_div::getIndpEnv('REMOTE_ADDR'), $TYPO3_CONF_VARS['SYS']['devIPmask'])) {
-			return;
+	/**
+	 * @param $params
+	 * @param $reference
+	 * @return mixed
+	 */
+	public function addLoginNews(&$params, &$reference) {
+
+		$record = $this->generateRecord();
+		if (is_array($record)) {
+			$TYPO3_CONF_VARS['BE']['loginNews'][] = $record;
 		}
-		
-		$tx_onclicklogin_users = array();
-	
+
+	}
+
+	/**
+	 * @param $params
+	 * @param $reference
+	 */
+	public function manipulateSystemNews(&$params, &$reference) {
+		$record = $this->generateRecord();
+		if (is_array($record)) {
+			$params['systemNews'][] = $record;
+		}
+	}
+
+	protected function generateRecord() {
+		$_EXTCONF = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['oneclicklogin']);
+
+		if ($_EXTCONF['use_devIPmask'] && !t3lib_div::cmpIP(t3lib_div::getIndpEnv('REMOTE_ADDR'), $GLOBALS['TYPO3_CONF_VARS']['SYS']['devIPmask'])) {
+			return FALSE;
+		}
+
+		$tx_oneclicklogin_users = array();
+
 		$time = time();
 		$res = $GLOBALS['TYPO3_DB']->exec_SELECTquery(
 			'*',
@@ -23,19 +47,20 @@ class tx_oneclicklogin_hooks{
 				starttime < ' . $time . ' AND
 				(endtime = 0 OR endtime > ' . $time . ')',
 			'', 'username ASC');
-		while($beUser = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
-			$name = $beUser['realName'] ? $beUser['realName'] : $beUser['username'];
-			$tx_oneclicklogin_users[] = '<a href="#" onclick="TYPO3BackendLogin.switchToOpenId();$(\'t3-username\').value=\'' . $beUser['tx_openid_openid'] . '\';$(\'t3-login-form-outer\').parentNode.submit();TYPO3BackendLogin.showLoginProcess();return false;">' . $name . '</a>';
+		while ($beUser = $GLOBALS['TYPO3_DB']->sql_fetch_assoc($res)) {
+			$name = htmlspecialchars($beUser['realName'] ? $beUser['realName'] : $beUser['username']);
+			$tx_oneclicklogin_users[] = '<a href="#" onclick="TYPO3BackendLogin.switchToOpenId();$(\'t3-username\').value=\'' . htmlspecialchars($beUser['tx_openid_openid']) . '\';$(\'t3-login-form-outer\').parentNode.submit();TYPO3BackendLogin.showLoginProcess();return false;">' . $name . '</a>';
 		}
-		
-		if(count($tx_oneclicklogin_users)){
-			$TYPO3_CONF_VARS['BE']['loginNews'][] = array(
+
+		if (count($tx_oneclicklogin_users)) {
+			return array(
 				'date' => $_EXTCONF['text_date'],
 				'header' => $_EXTCONF['text_header'],
 				'content' => join(', ', $tx_oneclicklogin_users),
 			);
+		} else {
+			return FALSE;
 		}
-		
 	}
 
 }
